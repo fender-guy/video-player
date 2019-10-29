5e0f891e45b20908f17198f1403df5312d49bd4d
Jeremy Hitchcock <whatthefuture@gmail.com>
2019-10-29 14:08:08 -0400

    Updates

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# frankly-test
-test for frankly
--- src/redux/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux-immutable';
-import playlist from './playlist';
-
-export default combineReducers({
-  playlist
-});
